245164f78db4cfacd575cca5c57b127a01c0d0f4
Diego Espelho <diego.espelho@gmail.com>
2024-08-25 22:23:45 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 const btnMobile = document.getElementById("btn-mobile");
 
+const nav = document.getElementById("nav");
+const menuLinks = document.querySelectorAll(".header-menu a");
+
 function toggleMenu(event) {
   if (event.type === "touchend") event.preventDefault();
   const nav = document.getElementById("nav");
@@ -12,6 +15,13 @@ function toggleMenu(event) {
     event.currentTarget.setAttribute("aria-label", "Abrir Menu");
   }
 }
+function closeMenu() {
+  nav.classList.remove("active");
+  btnMobile.setAttribute("aria-expanded", "false");
+  btnMobile.setAttribute("aria-label", "Abrir Menu");
+}
 
 btnMobile.addEventListener("click", toggleMenu);
 btnMobile.addEventListener("touchend", toggleMenu);
+
+menuLinks.forEach(link => link.addEventListener("click", closeMenu));
