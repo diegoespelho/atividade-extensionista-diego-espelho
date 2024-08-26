3e557bce79622863f1991f277cb8281ca9dcb937
Diego Espelho <diego.espelho@gmail.com>
2024-08-26 05:10:24 -0300

    Tentativa de resolver problema com menu mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,23 @@
 const btnMobile = document.getElementById("btn-mobile");
-
 const nav = document.getElementById("nav");
 const menuLinks = document.querySelectorAll(".header-menu a");
 
 function toggleMenu(event) {
-  if (event.type === "touchend") event.preventDefault();
-  const nav = document.getElementById("nav");
+  event.preventDefault();
   nav.classList.toggle("active");
   const active = nav.classList.contains("active");
-  event.currentTarget.setAttribute("aria-expanded", active);
-  if (active) {
-    event.currentTarget.setAttribute("aria-label", "Fechar Menu");
-  } else {
-    event.currentTarget.setAttribute("aria-label", "Abrir Menu");
-  }
+  btnMobile.setAttribute("aria-expanded", active);
+  btnMobile.setAttribute("aria-label", active ? "Fechar Menu" : "Abrir Menu");
+  // if (event.type === "touchend") event.preventDefault();
+  // const nav = document.getElementById("nav");
+  // nav.classList.toggle("active");
+  // const active = nav.classList.contains("active");
+  // event.currentTarget.setAttribute("aria-expanded", active);
+  // if (active) {
+  //   event.currentTarget.setAttribute("aria-label", "Fechar Menu");
+  // } else {
+  //   event.currentTarget.setAttribute("aria-label", "Abrir Menu");
+  // }
 }
 function closeMenu() {
   nav.classList.remove("active");
@@ -22,6 +26,6 @@ function closeMenu() {
 }
 
 btnMobile.addEventListener("click", toggleMenu);
-btnMobile.addEventListener("touchend", toggleMenu);
+// btnMobile.addEventListener("touchend", toggleMenu);
 
 menuLinks.forEach(link => link.addEventListener("click", closeMenu));
