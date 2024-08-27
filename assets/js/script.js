1fffda63c9e4ef222e7ef9a7f830c2ccedbd0047
Diego Espelho <diego.espelho@gmail.com>
2024-08-26 21:11:57 -0300

    sobre nós

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,16 +8,7 @@ function toggleMenu(event) {
   const active = nav.classList.contains("active");
   btnMobile.setAttribute("aria-expanded", active);
   btnMobile.setAttribute("aria-label", active ? "Fechar Menu" : "Abrir Menu");
-  // if (event.type === "touchend") event.preventDefault();
-  // const nav = document.getElementById("nav");
-  // nav.classList.toggle("active");
-  // const active = nav.classList.contains("active");
-  // event.currentTarget.setAttribute("aria-expanded", active);
-  // if (active) {
-  //   event.currentTarget.setAttribute("aria-label", "Fechar Menu");
-  // } else {
-  //   event.currentTarget.setAttribute("aria-label", "Abrir Menu");
-  // }
+  
 }
 function closeMenu() {
   nav.classList.remove("active");
@@ -26,6 +17,5 @@ function closeMenu() {
 }
 
 btnMobile.addEventListener("click", toggleMenu);
-// btnMobile.addEventListener("touchend", toggleMenu);
 
 menuLinks.forEach(link => link.addEventListener("click", closeMenu));
